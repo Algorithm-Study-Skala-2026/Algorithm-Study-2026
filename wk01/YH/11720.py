6efe1a79b3be774bd8ff612fd9cd528f309a599b
younghoon41 <yhlee0332@gmail.com>
2026-03-01 18:06:11 +0900

    ADD : 2026-03-01

---
+++ wk01/YH/11720.py
@@ -0,0 +1,13 @@
+# 숫자 합 구하기 문제
+
+n = int(input())
+numbers = list(input())
+
+sum = 0
+for i in numbers:
+    sum += int(i)
+
+print(sum)
+
+# 입력받은 숫자 문자열을 list()로 한 자리씩 나누어 저장한 뒤, 각 문자를 int()로 변환하며 누적 합산하는 방식으로 해결.
+# 별도의 알고리즘 없이 단순 순회(iteration)만으로 풀 수 있는 문제 였다고 생각함.
---
+++ wk01/YH/17298.py
@@ -0,0 +1,26 @@
+# 오큰수 구하기 문제
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+A = list(map(int, input().split()))
+
+ans = [0] * n
+myStack = []
+
+for i in range(n):
+
+    while myStack and A[myStack[-1]] < A[i]:
+        ans[myStack.pop()] = A[i]
+    myStack.append(i)
+
+while myStack:
+    ans[myStack.pop()] = -1
+
+print(*ans)
+
+
+# N이 최대 1,000,000으로 이중 반복문을 사용하면 시간 초과가 발생하므로, 스택(stack)을 활용하여 O(N)에 해결(강의 영상 참고함).
+# 스택에는 값이 아닌 인덱스를 저장하며, 새 원소가 스택 top의 수열값보다 크면 top을 pop하고 해당 인덱스의 정답을 현재 값으로 채움.
+# 이후 스택에 남은 인덱스는 오큰수가 존재하지 않으므로 -1을 저장.
