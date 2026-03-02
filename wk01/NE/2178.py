d73ceecb97a0085b7db1236da217a782a3c38393
강나은 <kkamssy02@gmail.com>
2026-03-02 22:05:56 +0900

    add

--- wk01/NE/2178.py
+++ wk01/NE/2178.py
@@ -3,7 +3,7 @@ from collections import deque
 
 input = sys.stdin.readline
 n, m = map(int, input().split())
-map = [list(map(int, input().strip())) for _ in range(n)]
+grid = [list(map(int, input().strip())) for _ in range(n)]
 chk = [[False] * m for _ in range(n)]
 
 dy = [0, 1, 0, -1]
@@ -13,13 +13,17 @@ def bfs(y,x):
     q = deque()
     q.append((y, x))
     chk[y][x] = True
-    
 
+    while q:
+        ey, ex = q.popleft()
+        for k in range(4):
+            ny = ey + dy[k]
+            nx = ex + dx[k]
+            if 0<=ny<n and 0<=nx<m:
+                if grid[ny][nx] == 1 and chk[ny][nx] == False:
+                    chk[ny][nx] = True
+                    q.append((ny, nx))
+                    grid[ny][nx] = grid[ey][ex] + 1
+    return grid[n-1][m-1]
 
-for j in range(n):
-    for i in range(m):
-        if map[j][i] == 1 and chk[j][i] == False:
-            chk[j][i] = True
-            minv = min(minv, bfs(j,i))
-
-print(minv)
+print(bfs(0,0))
