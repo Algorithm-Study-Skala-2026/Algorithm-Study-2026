a404e303e550e40c5b50b55f34ad743edee8c3ee
now-brother <nomagold57@gmail.com>
2026-03-05 11:11:56 +0900

    ADD : 2026-03-05

---
+++ wk02/JH/stack&queue/올바른괄호/Solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c == ')'){
+                if(!stack.isEmpty()) stack.pop();
+                else return false;
+            }else{
+                stack.add('(');
+            }
+        }
+        
+        if(!stack.isEmpty()) return false;
+        return true;
+    }
+}
