b03d8a2280c8da715005a75b11558063a010c9ec
Yujin <sheenie5225@gmail.com>
2026-03-29 23:38:07 +0900

    ADD: 2026-03-01 백준 #2178 미로탐색

---
+++ wk05/YJ/미로탐색.py
@@ -0,0 +1,27 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+graph = [list(map(int, input().strip())) for _ in range(N)]
+
+directions = [(-1,0),(1,0),(0,-1),(0,1)]
+
+def bfs():
+    queue = deque()
+    queue.append((0, 0))
+
+    while queue:
+        x, y = queue.popleft()
+
+        for dx, dy in directions:
+            nx, ny = x + dx, y + dy
+
+            if 0 <= nx < N and 0 <= ny < M:
+                if graph[nx][ny] == 1:
+                    graph[nx][ny] = graph[x][y] + 1
+                    queue.append((nx, ny))
+
+    return graph[N-1][M-1]
+
+print(bfs())
